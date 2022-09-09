8afc2d42e7881ea90e2c8388ebc8202dc8c5d45f
Taravann Uy <uytaravann18@kit.edu.kh>
2022-09-09 13:02:16 -0700

    commit 1

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <SignIn />
+  </div>
+</template>
+
+<script>
+import SignIn from './components/SignIn.vue'
+export default {
+  name: 'app',
+  components: {
+    SignIn
+  }
+}
+</script>
+
+<style lang="scss">
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    font-family: Playfair Display;
+    font-size: 1.6rem;
+    color: #222;
+    background-color: #FFFFFF;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
